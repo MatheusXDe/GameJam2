0de2bd81559bb194a0291dd15dd12c126c8575fe
MatheusXDe <daperdomo52@gmail.com>
2025-05-20 11:21:36 -0500

    win condition and pizza panel

--- Assets/GameJam/David/Scripts/PizzaPanel.cs
+++ Assets/GameJam/David/Scripts/PizzaPanel.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,10 +12,15 @@ public class PizzaPanel : MonoBehaviour
     public int currPizza = -1; Pizza currP;
 
     public bool hasWin;
-    [SerializeField] UnityEvent onUIOrder, onUIPizza;
-    private void Start()
+    [SerializeField] UnityEvent onUIOrder, onUIPizza,onUIWin;
+    public IEnumerator FirstInvoke()
     {
-        onUIOrder?.Invoke();
+        yield return new WaitForSeconds(.1f);
+        onUIOrder.Invoke();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) SetPizzaPosition();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -58,13 +64,14 @@ public class PizzaPanel : MonoBehaviour
         switch (currPizza)
         {
             case 0: //Top Pizza
-
+                pizzaPart[0].transform.position = new Vector2(transform.position.x,pizzaPart[0].transform.position.y);
                 break;
             case 1: // Center Pizza
-
+                pizzaPart[1].transform.position = new Vector2(transform.position.x, pizzaPart[1].transform.position.y);
                 break;
             case 2: // Bottom Pizza
-
+                pizzaPart[2].transform.position = new Vector2(transform.position.x, pizzaPart[2].transform.position.y);
+                WinCondition();
                 break;
             default:
                 break;
@@ -75,5 +82,6 @@ public class PizzaPanel : MonoBehaviour
     void WinCondition()
     {
         hasWin = pizzaPart[0].type == order[0] && pizzaPart[1].type == order[1] && pizzaPart[2].type == order[2];
+        onUIWin?.Invoke();
     }
 }
